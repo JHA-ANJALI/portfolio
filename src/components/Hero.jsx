95d3abfb0386bdbe99171abc13b69e73e6fb3418
JHA-ANJALI <anjalijhabithuar2003@gmail.com>
2025-02-28 17:54:00 +0530

    ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export default function Hero() {
                     <div className="flex items-center gap-3">
                         <figure className="img-box w-9 h-9 rounded-lg">
                             <img
-                                src="/images/pp.png" // ✅ Corrected path
+                                src="/assets/pp.png" // ✅ Corrected path
                                 width={40}
                                 height={40}
                                 alt="anjali"

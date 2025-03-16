1a90ee6a1a7e84fce38ac4dcf9743257b2f4f25b
JHA-ANJALI <anjalijhabithuar2003@gmail.com>
2025-03-16 16:19:31 +0530

    ok

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ import netflixoropic from "./netflixpropic.png"
 export default function Work() {
   const works = [
     { imgSrc: portfoliopropic, title: 'Portfolio', tags: ['react.js', 'tailwind css'], projectLink: 'https://jha-anjali.github.io/portfolio/' },
-    { imgSrc: netflixoropic, title: 'Netflix Clone', tags: ['API', 'Node.js' ,'react.js'], projectLink: 'https://pixstock-official.vercel.app/' },
+    { imgSrc: netflixoropic, title: 'Netflix Clone', tags: ['API', 'Node.js' ,'react.js'], projectLink: 'https://github.com/JHA-ANJALI/MyNetflixClone' },
     // { imgSrc: '/images/project-3.jpg', title: 'Recipe app', tags: ['Development', 'API'], projectLink: '' },
     // { imgSrc: '/images/project-4.jpg', title: 'Real estate website', tags: ['Web-design', 'Development'], projectLink: 'https://github.com/codewithsadee-org/wealthome' },
     // { imgSrc: '/images/project-5.jpg', title: 'eCommerce website', tags: ['eCommerce', 'Development'], projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website' },

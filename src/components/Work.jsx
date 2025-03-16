759e669045f8e66ad371e1f37c8b1f91f2e84e1c
JHA-ANJALI <anjalijhabithuar2003@gmail.com>
2025-03-16 16:12:02 +0530

    ok

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import portfoliopropic from "./PORTFOLIOPROPIC.png"
+import netflixoropic from "./netflixpropic.png"
 
 export default function Work() {
   const works = [
-    { imgSrc: {portfoliopropic}, title: 'Full stack music app', tags: ['REACT.JS', 'TAILWIND CSS'], projectLink: 'https://jha-anjali.github.io/portfolio/' },
-    { imgSrc: '/images/project-2.jpg', title: 'Free stock photo app', tags: ['API', 'SPA'], projectLink: 'https://pixstock-official.vercel.app/' },
-    { imgSrc: '/images/project-3.jpg', title: 'Recipe app', tags: ['Development', 'API'], projectLink: '' },
-    { imgSrc: '/images/project-4.jpg', title: 'Real estate website', tags: ['Web-design', 'Development'], projectLink: 'https://github.com/codewithsadee-org/wealthome' },
-    { imgSrc: '/images/project-5.jpg', title: 'eCommerce website', tags: ['eCommerce', 'Development'], projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website' },
-    { imgSrc: '/images/project-6.jpg', title: 'vCard Personal portfolio', tags: ['Web-design', 'Development'], projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio' }
+    { imgSrc: portfoliopropic, title: 'Portfolio', tags: ['react.js', 'tailwind css'], projectLink: 'https://jha-anjali.github.io/portfolio/' },
+    { imgSrc: netflixoropic, title: 'Netflix Clone', tags: ['API', 'Node.js' ,'react.js'], projectLink: 'https://pixstock-official.vercel.app/' },
+    // { imgSrc: '/images/project-3.jpg', title: 'Recipe app', tags: ['Development', 'API'], projectLink: '' },
+    // { imgSrc: '/images/project-4.jpg', title: 'Real estate website', tags: ['Web-design', 'Development'], projectLink: 'https://github.com/codewithsadee-org/wealthome' },
+    // { imgSrc: '/images/project-5.jpg', title: 'eCommerce website', tags: ['eCommerce', 'Development'], projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website' },
+    // { imgSrc: '/images/project-6.jpg', title: 'vCard Personal portfolio', tags: ['Web-design', 'Development'], projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio' }
   ];
 
   return (
